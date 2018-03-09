79eb1b628dbafa3e0f23e26bcf4c715c09b45aec
Kris Madsen <jkrismadsen@gmail.com>
2018-03-08 22:34:55 -0500

    provide an alternative, possibly less confusing way to add quereis and headers

--- avapi.py
+++ avapi.py
@@ -10,15 +10,19 @@ import avapi
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-g', '--get', nargs='+', action='store')
+parser.add_argument('-H', '--headers', type=str)
 parser.add_argument('-k', '--key', type=str)
-parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('-q', '--queries', type=str)
 parser.add_argument('-s', '--stringify', action='store_true')
+parser.add_argument('-v', '--verbose', action='store_true')
 
 args = parser.parse_args()
 
 av = avapi.Avapi(
+                 headers=util.parser.args_to_dict(args.headers),
                  key=args.key,
-                 verbose=args.verbose
+                 queries=util.parser.args_to_dict(args.queries),
+                 verbose=args.verbose,
                 )
 
 get_functions = {
@@ -31,6 +35,8 @@ get_functions = {
     'batch'           : av.batch
 }
 
+result = None
+
 if args.get is not None:
     if args.get[0] != 'intraday':
         pargs = [args.get[1]] + list(map(lambda x: util.parser.args_to_dict(x), args.get[2:]))
@@ -38,6 +44,13 @@ if args.get is not None:
     else:
         pargs = args.get[1:3] + list(map(lambda x: util.parser.args_to_dict(x), args.get[3:]))
         result = get_functions[args.get[0]](*pargs)
+
+if result is not None:
     if args.stringify:
         result = json.dumps(result)
+
     pprint(result)
+
+else:
+    print 'Nothing bad seems to have happened.  But then again, nothing good seems to have happened either.'
+    exit()
--- util/parser.py
+++ util/parser.py
@@ -1,7 +1,11 @@
 def args_to_dict(args):
     ret = {}
-    arg_list = args.split(',')
-    for arg in arg_list:
-        kv = arg.split('=')
-        ret[kv[0]] = kv[1]
+    try:
+        arg_list = args.split(',')
+        for arg in arg_list:
+            if arg != '' and arg != None and arg != 'None':
+                kv = arg.split('=')
+                ret[kv[0]] = kv[1]
+    except:
+        print 'Could not parse additional arguments'
     return ret
